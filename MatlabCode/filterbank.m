22f639cf5a8426cf3bf76392d39f4faaf29fd3c0
luis-vieira-luis <luispedrovieira@gmail.com>
2017-10-13 15:57:19 +0200

    Update filterbank.m

--- MatlabCode/filterbank.m
+++ MatlabCode/filterbank.m
@@ -1,16 +1,16 @@
 function fb = filterbank(sig,N,M,g)
 % Filter bank with N parallel combfilters
 % N : order of the filter bank
-% del : matrix with z(n) delay lenths
+% M : matrix with z(n) delay lenths
 % g : gain
 
-Stack = [];%zeros(3,6500);
+Stack = [];
 
 for i = 1:1:N
 
     R = delayZ(sig(i),M(i),g); % recurssive part of feedback combfilter
 
-    zeros_pad = zeros(1,6500 - length(R));
+    zeros_pad = zeros(1,6500 - length(R)); % buffer of 6500 samples maximum (random value for the moment)
     R_pad = [R zeros_pad];
 
     Stack(i,:) = [R_pad]; % store in matrix the values of the recurssive part
