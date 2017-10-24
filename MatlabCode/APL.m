4a5c252ec8de0d7a9e50a4ceb425e416dd327b3d
Luís Vieira <luispedrovieira@h1779.eduroam-5.wlan.aalborguniversitet.dk>
2017-10-24 17:42:57 +0200

    Update 4.0: New FDN + Absorbent Allpass

--- MatlabCode/APL.m
+++ MatlabCode/APL.m
@@ -1,38 +1,16 @@
-function out = APL(M_length,Tr,HFratio,Diffusion,fc,Fs,in)
+function out = APL(in,M,k,g,at)
 % absorbent allpass filter
-% M_length : delay length in ms
-% fc : cut-off frequency
-% HFRatio : ratio of the decay time at high freq
-% att : attenuation coefficient (associated with delay and LPF)
+% M : delay length in samples
+% at : attenuation coefficient (associated with delay and LPF)
 % g : feedback and feedforward coefficient
-
-%------------------------------------------------------------------------%
-% calculate coeff 'b'
-
-maxallpass = 0.61803; % solution to 1−x^2 = x
-g = maxallpass*(Diffusion/100); % All-Pass Coefficient
-
-aDb = -60*(M_length/Tr); % attenuation for T60
-att = 10.^(aDb/20); % Absorbent Gain
-
-dBGainFC = -60*(M_length/(HFratio*Tr));
-G = 10.^(dBGainFC/20); %
-
-
-if G == 1.0
-    B = 0.0;
-else
-    omega = cos(2*pi*(fc/Fs));
-    A = 8*G - 4*G*G-9*G*omega + 4*G*G*omega*omega;
-    B = (2*G*omega - 2 + sqrt(A))/(2*G-2);
-end
+% k : coefficient lowpass filter
 
 %------------------------------------------------------------------------%
 % creating filter
 
-z_padding = zeros(1, (M_length-3));
-b = [g -g*B z_padding (att-att*B)];
-a = [1 -B  z_padding (-g*att-g*att*B)];
+z_padding = zeros(1, (M-3));
+b = [g z_padding (-g)*k (at-at*k)];
+a = [1 z_padding -k (g*at-g*at*k)];
 out = filter(b, a, in);
 
 
--- MatlabCode/main.m
+++ MatlabCode/main.m
@@ -1,64 +1,53 @@
 %% ------ JOT REVERBERATOR ------ %%
 clear all; close all;
 %%
+%-----------------------------------------------------------------------------%
+% Initialize variables
+%-----------------------------------------------------------------------------%
 
 load 'jungle.mat'
 
 Fs = 44100;
-Ns = 2048; % number of points for fft
+
 
 sig = [1; zeros(3999,1)];
 %[sig,fs] = audioread('flute_music.wav');
-sig1 = jungle_L; % original sig
+%sig = jungle_L; % original sig
 
+Ns = 2048; % number of points for fft
 
-sig_fft = fft(sig1,Ns); % fft analysis
+sig_fft = fft(sig,Ns); % fft analysis
 freq = (0:Ns-1)*(Fs/Ns); % scaling frequency range
 
 mag = abs(sig_fft);
 mag_db = 20*log10(mag);
 
-%-----------------------------------------------------------------------------%
-%% Plotting
 
-figure(3)
-subplot(311)
-plot(sig)
-title('ImpRes - Time Domain (samples)')
-
-subplot(312)
-plot(sig1)
-title('Jungle - Time Domain (samples)')
-
-subplot(313)
-semilogx(freq,mag_db)
-axis([20 20000 -100 0])
-title('Jugle - FFT')
-grid on
+Tr = 2000;  % decay time in ms
+Diffusion  = 100;
 
 %-----------------------------------------------------------------------------%
-%% Early Reflections
+% Early Reflections
 %-----------------------------------------------------------------------------%
 
 % Lowpass filter
 % attenuation at high frequency
 roomHighFreq = 3000;
 LPF = Lowpass(sig,roomHighFreq,Fs);
-%%
+
 %-----------------------------------------------------------------------------%
 % Delay lines
 % in samples
 
-del = [661 970 1323 4410]; % tap delaylines values
+delayline = [661 970 1323 4410]; % tap delaylines values
 bn = [0.8 0.5 0.3 0.1];     % delay attenuation coefficients
 
 Z = [];
-zeropad = max(del);
-bufflength = length(LPF)+zeropad;
+maxldel = max(delayline);
+bufflength = length(LPF)+maxldel;
 
-%%
 for i = 1: 4
-    y = delayZ(LPF,del(i),bn(i)); % delayline
+    y = delayZ(LPF,delayline(i),bn(i)); % delayline
     zpad = zeros(1,bufflength-length(y));
     y1 = [y zpad];
     Z(i,:) = [y1]; % store delaylines
@@ -70,90 +59,84 @@ TDL = sum(Z)';
 %-----------------------------------------------------------------------------%
 % Allpass filter
 
-g = 0.4;
-N = 2;
-APF = Allpass(TDL,g,N);
+gainAllpass = 0.4;
+orderAllpass = 2;
+APF = Allpass(TDL,gainAllpass,orderAllpass);
 
 %-----------------------------------------------------------------------------%
 early_ref = APF*0.6;    % early reflections output
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 %-----------------------------------------------------------------------------%
 %% FEEDBACK DELAY NETWORK
 %-----------------------------------------------------------------------------%
-% H(z) = N / (3 - Sum(g*Z(M)))
-% g = 0.7;
-% A = g/(sqrt(2)).*[0 1 1 0; -1 0 0 -1; 1 0 0 -1; 0 1 -1 0]; % feedback matrix gains
-% M = [244 353 457 1452]; % delay lengths
-% bn = [0.8 0.5 0.3 0.3]; % gain coeff before delay lines
-% cn = bn; % gain coeff after delay lines
-% qn = [0.4 0.4 0.4 0.4];
-% d = 0.5; % gain coeff direct signal
-% D = diag(M); % diagonal matrix with delay lenghts
+
+gainFDN = 0.6;
+cn = [0.8 0.8 0.8 0.8];
+d = 0.4;
+
+gain = gainFDN/sqrt(2);
+a = gain.*[0 1 1 0; -1 0 0 -1; 1 0 0 -1; 0 1 1 0]; % Feedback matrix
+
+%A = [a -a -a -a; -a a -a -a; -a -a a -a; -a -a -a a];
+
+sig = sig';
+
+
+
+m = delayline;
+gainAbsorbent = 0.5;
+at = 0.9; % attenuation
+
 %
-% H = bn.*(cn'.*(D-A))+d;
-
-
-for i = 1:length(Z)
-    
-FDN = filterbank(Z(i),M(i),g_M(i));
-
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%-----------------------------------------------------------------------------%
-%% LATE REVERBERATION
-%-----------------------------------------------------------------------------%
-% Absorbent Allpass filters in series
-
-Mslength = [78189 53096 39469 28047 18036 14200 6615]; % absorbent allpass delay length
-M_length = [1772 1203 894 635 409 321 150]; % delay lengths in ms
-Tr = 2000; % decay time in ms
-fc = 18000; % absorbent allpass cut-off frequency (HF reference value)
+M = [6615 14200 18036 28047 39469 53096 78189]; % absorbent allpass delay length
+Tr = 2000;  % decay time in ms
 Diffusion  = 100;
-HFratio = 0.3;
+kn = [0.9 0.6 0.5 0.4 0.6 0.3 0.2];
 
-apl = []; % initialize vector
-
-for i = 1:length(Mslength)
-
-    abf = APL(M_length(i),Tr,HFratio,Diffusion,fc,Fs,FDN);
-    apl(i,:) = [abf];
+%%
+for i = 1:4 
+    for j = 1:length(sig)
+        h = APL(Z(i,j),M(i),kn(i),gainAbsorbent,at);
+        H(i,j) = [h];
+    end
 end
+
+
+%%
+% filterbank
+
+% Delaylines
+z1 = H(1,:);
+z2 = H(2,:);
+z3 = H(3,:);
+z4 = H(4,:);
+
+% for n = 1:length(sig)
+%     
+%     tmp = [z1(m(1)) z2(m(2)) z3(m(3)) z4(m(4))];
+%     
+%     fdn(n) = sig(n) + cn(1)*z1(m(1))+ cn(2)*z2(m(2)) ...
+%         + cn(3)*z3(m(3)) + cn(4)*z4(m(4));
+%     
+%     z1 = [(sig(n)*bn(1) + tmp*a(1,:)') z1(1:length(z1)-1)];
+%     z2 = [(sig(n)*bn(2) + tmp*a(2,:)') z2(1:length(z2)-1)];
+%     z3 = [(sig(n)*bn(3) + tmp*a(3,:)') z3(1:length(z3)-1)];
+%     z4 = [(sig(n)*bn(4) + tmp*a(4,:)') z4(1:length(z4)-1)];
+%     
+% end
+
+for n = 1:length(sig)
+    
+    tmp = [z1(m(1)) z2(m(2)) z3(m(3))];
+    
+    fdn(n) = sig(n) + cn(1)*z1(m(1))+ cn(2)*z2(m(2)) ...
+        + cn(3)*z3(m(3));
+    
+    z1 = [(sig(n)*bn(1) + tmp*a(1,3)') z1(1:length(z1)-1)];
+    z2 = [(sig(n)*bn(2) + tmp*a(2,3)') z2(1:length(z2)-1)];
+    z3 = [(sig(n)*bn(3) + tmp*a(3,3)') z3(1:length(z3)-1)];
+    
+end
+
